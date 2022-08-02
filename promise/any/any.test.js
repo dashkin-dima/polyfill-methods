d63c465df31f8b15f0c817fb8841fde92ec86fd5
dgdashkin <dgdashkin@sberbank.ru>
2022-08-02 20:53:36 +0300

    bugfix: поправил тест

--- promise/any/any.test.js
+++ promise/any/any.test.js
@@ -4,24 +4,24 @@ import { any } from "./index";
 describe("promise any", () => {
   test("resolve first promise", () => {
     const firstResolvePromise = createPromise(1, "first");
-    const secondResolvePromise = createPromise(1, "second");
+    const secondResolvePromise = createPromise(2, "second");
     expect(any([firstResolvePromise, secondResolvePromise])).resolves.toBe(
       "first"
     );
   });
 
   test("resolve any when reject someone promise", () => {
-    const firstResolvePromise = createPromise(1, "first", false);
-    const secondResolvePromise = createPromise(1, "second");
-    expect(any([firstResolvePromise, secondResolvePromise])).resolves.toBe(
-      "first"
+    const firstRejectPromise = createPromise(1, "first", false);
+    const secondResolvePromise = createPromise(2, "second");
+    expect(any([firstRejectPromise, secondResolvePromise])).resolves.toBe(
+      "second"
     );
   });
 
   test("reject when reject all promise", () => {
-    const firstResolvePromise = createPromise(1, "first", false);
-    const secondResolvePromise = createPromise(1, "second", false);
-    expect(any([firstResolvePromise, secondResolvePromise])).rejects.toBe([
+    const firstRejectPromise = createPromise(1, "first", false);
+    const secondRejectPromise = createPromise(1, "second", false);
+    expect(any([firstRejectPromise, secondRejectPromise])).rejects.toEqual([
       "first",
       "second",
     ]);
